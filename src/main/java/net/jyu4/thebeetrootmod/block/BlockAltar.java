80b52eb51b883c27026db1741c6e6ccc4de79328
Jyu4 <jyu4@outlook.de>
2024-05-23 14:40:55 +0800

    commit

--- src/main/java/net/jyu4/thebeetrootmod/block/BlockAltar.java
+++ src/main/java/net/jyu4/thebeetrootmod/block/BlockAltar.java
@@ -2,6 +2,8 @@ package net.jyu4.thebeetrootmod.block;
 
 import net.jyu4.thebeetrootmod.block.blockentity.BlockEntityAltar;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -16,5 +18,9 @@ public class BlockAltar extends BlockBase implements EntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
         return new BlockEntityAltar(pPos, pState);
+    }
+
+    public BlockState updateShape(BlockState pState, Direction pDirection, BlockState pNeighborState, LevelAccessor pLevel, BlockPos pPos, BlockPos pNeighborPos) {
+        return super.updateShape(pState, pDirection, pNeighborState, pLevel, pPos, pNeighborPos);
     }
 }
